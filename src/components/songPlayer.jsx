16a379650d650c43704a165664fb1590c4ab5f62
Janavi Arora <janaviarora55@gmail.com>
2026-03-09 16:31:34 +0530

    made some changes

--- src/components/songPlayer.jsx
+++ src/components/songPlayer.jsx
@@ -1,20 +1,20 @@
 import { useState, useRef } from "react";
 import "./styles.css";
 
-// album covers
+
 import img1 from "../assets/1.jpg";
 import img2 from "../assets/2.jpg";
 import img3 from "../assets/3.jpg";
 import img4 from "../assets/4.jpg";
 import img5 from "../assets/5.jpg";
 
-// icons
+
 import prevIcon from "../assets/previous.png";
 import nextIcon from "../assets/next.png";
 import playIcon from "../assets/play.png";
 import pauseIcon from "../assets/pause.png";
 
-// songs
+
 import iris from "../songs/iris.mp3";
 import eyes from "../songs/eyes.mp3";
 import aboutYou from "../songs/aboutYou.mp3";
@@ -28,12 +28,11 @@ export default function SongPlayer() {
 
   const [currentSong, setCurrentSong] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [progress, setProgress] = useState(0);
+
 
   const audioRef = useRef(null);
 
   function togglePlay() {
-
     if (isPlaying) {
       audioRef.current.pause();
     } else {
@@ -44,7 +43,6 @@ export default function SongPlayer() {
   }
 
   function nextSong() {
-
     const next = (currentSong + 1) % songs.length;
 
     setCurrentSong(next);
@@ -56,7 +54,6 @@ export default function SongPlayer() {
   }
 
   function prevSong() {
-
     const prev = (currentSong - 1 + songs.length) % songs.length;
 
     setCurrentSong(prev);
@@ -67,26 +64,8 @@ export default function SongPlayer() {
     }, 0);
   }
 
-  function updateProgress() {
-
-    const { currentTime, duration } = audioRef.current;
-
-    const percent = (currentTime / duration) * 100;
-
-    setProgress(percent || 0);
-  }
-
-  function seek(e) {
-
-    const width = e.currentTarget.clientWidth;
-    const clickX = e.nativeEvent.offsetX;
-    const duration = audioRef.current.duration;
-
-    audioRef.current.currentTime = (clickX / width) * duration;
-  }
 
   function handleEnded() {
-
     const next = (currentSong + 1) % songs.length;
 
     setCurrentSong(next);
@@ -101,13 +80,6 @@ export default function SongPlayer() {
 
       <div className="img-container">
         <img src={images[currentSong]} alt="Song cover" />
-      </div>
-
-      <div className="progress-container" onClick={seek}>
-        <div
-          className="progress"
-          style={{ width: `${progress}%` }}
-        ></div>
       </div>
 
       <div className="menu">
